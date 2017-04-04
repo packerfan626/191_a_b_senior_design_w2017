865d6c26255b2513b1d023a0b2cba3b503177c0d
packerfan626 <dori_mouawad@hotmail.com>
2017-04-03 19:23:24 -0700

    Basic buttons added

--- app/src/main/java/millionkids/millionkidseducation/Children/ChildrenHome.java
+++ app/src/main/java/millionkids/millionkidseducation/Children/ChildrenHome.java
@@ -2,10 +2,13 @@ package millionkids.millionkidseducation.Children;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 import millionkids.millionkidseducation.R;
 import millionkids.millionkidseducation.menuUI.About;
@@ -15,6 +18,9 @@ import millionkids.millionkidseducation.menuUI.Settings;
 
 public class ChildrenHome extends AppCompatActivity {
 
+    //ImageButton declarations
+    ImageButton girl5_8, boy5_8, girl9_12, boy9_12, girl13_17, boy13_17;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,25 @@ public class ChildrenHome extends AppCompatActivity {
 
         //Set screen orientation to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        
+
+        //SetButtons for image buttons based on UI
+        girl5_8 = (ImageButton)findViewById(R.id.girl5_8);
+        boy5_8 = (ImageButton)findViewById(R.id.boy5_8);
+        girl9_12 = (ImageButton)findViewById(R.id.girl9_12);
+        boy9_12 = (ImageButton)findViewById(R.id.boy9_12);
+
+//        //SetImages for buttons
+//        girl5_8.setBackgroundResource(R.drawable.girl5_8);
+//        girl5_8.setAlpha(1.0f);
+//
+//        boy5_8.setBackgroundResource(R.drawable.boy5_8);
+//        boy5_8.setAlpha(1.0f);
+//
+//        girl9_12.setBackgroundResource(R.drawable.girl9_12);
+//        girl9_12.setAlpha(1.0f);
+//
+//        boy9_12.setBackgroundResource(R.drawable.boy9_12);
+//        boy9_12.setAlpha(1.0f);
     }
 
     //Menu option BEGINS
