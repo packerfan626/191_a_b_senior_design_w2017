b25ffa56bb4fa1f2b04a7843ab0dc580dcad8cee
packerfan626 <dori_mouawad@hotmail.com>
2017-04-04 21:04:45 -0700

    Setting up for dynamically created buttons in ScenarioOptions screen.

--- app/src/main/java/millionkids/millionkidseducation/Children/ScenarioOptions.java
+++ app/src/main/java/millionkids/millionkidseducation/Children/ScenarioOptions.java
@@ -4,15 +4,25 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import millionkids.millionkidseducation.R;
 
 public class ScenarioOptions extends AppCompatActivity {
 
+    //LinearLayout declaration
+    LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario_options);
+
+        //Linking LinearLayout to UI
+        linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
 
         //Set screen orientation to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -21,6 +31,32 @@ public class ScenarioOptions extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String folder = bundle.getString("folder");
 
-        Log.d("Button", "HERE");
+
+        //Open Folder and read contents
+
+
+        //End
+
+        //Testing Dynamic ImageViews in ScenarioOptions UI
+        ImageButton image = new ImageButton(this);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        image.setLayoutParams(lp);
+        image.setMaxHeight(300);
+        image.setMaxWidth(300);
+        image.setImageResource(R.drawable.boy5_8);
+        image.setAdjustViewBounds(true);
+        linearLayout.addView(image);
+
+        ImageButton image2 = new ImageButton(this);
+
+        image2.setLayoutParams(lp);
+        image2.setMaxHeight(300);
+        image2.setMaxWidth(300);
+        image2.setImageResource(R.drawable.boy9_12);
+        image2.setAdjustViewBounds(true);
+        linearLayout.addView(image2);
     }
 }
