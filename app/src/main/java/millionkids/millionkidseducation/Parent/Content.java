1fd3b40092dadfcae1f497e7442640df4fefbe15
cpoon4 <poonch@uci.edu>
2017-04-05 01:54:14 -0700

    MVP adult section

--- app/src/main/java/millionkids/millionkidseducation/Parent/Content.java
+++ app/src/main/java/millionkids/millionkidseducation/Parent/Content.java
@@ -6,14 +6,100 @@ package millionkids.millionkidseducation.Parent;
 
 public class Content
 {
-    private String currContent = "";
+    private String returnSummaryContent = "",
+            returnGroomingContent = "",
+            returnWarningSignsContent = "",
+            returnPreventativeMeasure = "",
+            currentContent = "";
+    private final String SEX_TRAFFICKING_MINOR = "sex trafficking minor",
+            SEX_TRAFFICKING_ADULT = "sex trafficking adult",
+            LABOR_TRAFFICKING = "labor trafficking",
+            SEXTORTION = "sextortion",
+            CHILD_PORNOGRAPHY = "child pornography",
+            SOCIAL_MEDIA_EXPLOITATION = "social media exploitation";
 
-    public Content() { }
-
-    //create methods per informational contents
-    public String homeContent()
+    //constructor
+    public Content(String currentContent)
     {
-        currContent = "this is a test";
-        return currContent;
+        this.currentContent = currentContent;
+        setContents(currentContent);
+    }
+
+    //create methods to return information contents
+    public String summaryContent() {return returnSummaryContent; }
+    public String groomingProcessContent() { return returnGroomingContent; }
+    public String warningSignsContent() { return returnWarningSignsContent; }
+    public String preventativeMeasureContent() { return returnPreventativeMeasure; }
+
+    public String getCurrentContent() { return currentContent; }
+    //set methods
+    public void setcurrentContent(String currentContent)
+    {
+        this.currentContent = currentContent;
+        setContents(currentContent);
+    }
+
+    private void setContents(String currentContent)
+    {
+        if(currentContent.equalsIgnoreCase(SEX_TRAFFICKING_MINOR))
+            setSexTraffickingMinor();
+        else if (currentContent.equalsIgnoreCase(SEX_TRAFFICKING_ADULT))
+            setSexTraffickingAdult();
+        else if (currentContent.equalsIgnoreCase(LABOR_TRAFFICKING))
+            setLaborTrafficking();
+        else if (currentContent.equalsIgnoreCase(SEXTORTION))
+            setSextortion();
+        else if (currentContent.equalsIgnoreCase(CHILD_PORNOGRAPHY))
+            setChildPornography();
+        else if (currentContent.equalsIgnoreCase(SOCIAL_MEDIA_EXPLOITATION))
+            setSocialMediaExploitation();
+    }
+
+    private void setSexTraffickingMinor()
+    {
+        returnSummaryContent = "SEX_TRAFFICKING_MINOR Summary content";
+        returnGroomingContent = "SEX_TRAFFICKING_MINOR Grooming Process Content";
+        returnWarningSignsContent = "SEX_TRAFFICKING_MINOR Warning Signs Content";
+        returnPreventativeMeasure = "SEX_TRAFFICKING_MINOR Preventative Mesaure Content";
+    }
+
+    private void setSexTraffickingAdult()
+    {
+        returnSummaryContent = "SEX_TRAFFICKING_ADULT Summary content";
+        returnGroomingContent = "SEX_TRAFFICKING_ADULT Grooming Process Content";
+        returnWarningSignsContent = "SEX_TRAFFICKING_ADULT Warning Signs Content";
+        returnPreventativeMeasure = "SEX_TRAFFICKING_ADULT Preventative Mesaure Content";
+    }
+
+    private void setLaborTrafficking()
+    {
+        returnSummaryContent = "LABOR_TRAFFICKING Summary content";
+        returnGroomingContent = "SEX_TRAFFICKING_ADULT Grooming Process Content";
+        returnWarningSignsContent = "SEX_TRAFFICKING_ADULT Warning Signs Content";
+        returnPreventativeMeasure = "SEX_TRAFFICKING_ADULT Preventative Mesaure Content";
+    }
+
+    private void setSextortion()
+    {
+        returnSummaryContent = "SEXTORTION Summary content";
+        returnGroomingContent = "SEXTORTION Grooming Process Content";
+        returnWarningSignsContent = "SEXTORTION Warning Signs Content";
+        returnPreventativeMeasure = "SEXTORTION Preventative Mesaure Content";
+    }
+
+    private void setChildPornography()
+    {
+        returnSummaryContent = "CHILD_PORNOGRAPHY Summary content";
+        returnGroomingContent = "CHILD_PORNOGRAPHY Grooming Process Content";
+        returnWarningSignsContent = "CHILD_PORNOGRAPHY Warning Signs Content";
+        returnPreventativeMeasure = "CHILD_PORNOGRAPHY Preventative Mesaure Content";
+    }
+
+    private void setSocialMediaExploitation()
+    {
+        returnSummaryContent = "SOCIAL_MEDIA_EXPLOITATION Summary content";
+        returnGroomingContent = "SOCIAL_MEDIA_EXPLOITATION Grooming Process Content";
+        returnWarningSignsContent = "SOCIAL_MEDIA_EXPLOITATION Warning Signs Content";
+        returnPreventativeMeasure = "SOCIAL_MEDIA_EXPLOITATION Preventative Mesaure Content";
     }
 }
--- app/src/main/java/millionkids/millionkidseducation/Parent/ParentHome.java
+++ app/src/main/java/millionkids/millionkidseducation/Parent/ParentHome.java
@@ -1,30 +1,79 @@
 package millionkids.millionkidseducation.Parent;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import millionkids.millionkidseducation.R;
+import millionkids.millionkidseducation.main.MainHomePage;
 import millionkids.millionkidseducation.menuUI.About;
 import millionkids.millionkidseducation.menuUI.Help;
 import millionkids.millionkidseducation.menuUI.LearnMore;
 import millionkids.millionkidseducation.menuUI.Settings;
 
-public class ParentHome extends AppCompatActivity {
+public class ParentHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    private final String SEX_TRAFFICKING_MINOR = "sex trafficking minor",
+                        SEX_TRAFFICKING_ADULT = "sex trafficking adult",
+                        LABOR_TRAFFICKING = "labor trafficking",
+                        SEXTORTION = "sextortion",
+                        CHILD_PORNOGRAPHY = "child pornography",
+                        SOCIAL_MEDIA_EXPLOITATION = "social media exploitation";
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private TextView tvSectionName, tvCurrentContent;
+//    private String currentContent = "SexTraffickingMinor";
+    private Button bSummary, bGroomingProcess, bWarrningSigns, bPreventMeasure, prevButton;
+//    private Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     //class that handles all the parent information
-    Content con = new Content();
+    Content con = new Content(SEX_TRAFFICKING_MINOR);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_home);
 
-        final TextView tvWord = (TextView) findViewById(R.id.tvParent);
-        tvWord.setText(con.homeContent());
+        //set private variables
+        tvSectionName = (TextView) findViewById(R.id.tvSectionName);
+        tvCurrentContent = (TextView) findViewById(R.id.tvCurrentContent);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.Open, R.string.close);
+        bSummary = (Button) findViewById(R.id.bSummary);
+        bGroomingProcess = (Button) findViewById(R.id.bGroomingProcess);
+        bWarrningSigns = (Button) findViewById(R.id.bWarrningSigns);
+        bPreventMeasure = (Button) findViewById(R.id.bPreventMeasure);
+        prevButton = bSummary;
+
+        //set initial load
+        bSummary.setBackgroundColor(Color.BLUE);
+        tvCurrentContent.setText(con.summaryContent());
+
+        //Button Listeners
+        bSummary.setOnClickListener(this);
+        bGroomingProcess.setOnClickListener(this);
+        bWarrningSigns.setOnClickListener(this);
+        bPreventMeasure.setOnClickListener(this);
+
+        //set Navigation menu
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //listen for buttons in navigation Menu
+        NavigationView nv = (NavigationView) findViewById(R.id.navigationView);
+        nv.setNavigationItemSelectedListener(this);
+//        setTitle(con.getCurrentContent());
     }
 
     //Menu option BEGINS
@@ -33,14 +82,19 @@ public class ParentHome extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
-        //  return true;
     }
-//
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.mHome:
-                //this.startActivity(new Intent(this, MainHomePage.class));
+                this.startActivity(new Intent(this, MainHomePage.class));
                 break;
             case R.id.mAbout:
                 this.startActivity(new Intent(this, About.class));
@@ -58,4 +112,95 @@ public class ParentHome extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //Menu option ENDS
+
+    //Navigation Menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.mSexTraffickingMinor:
+//                currentContent = "SexTraffickingMinor";
+                con.setcurrentContent(SEX_TRAFFICKING_MINOR);
+                this.setTitle("Sex Trafficking - Minor");
+                tvSectionName.setText("Sex Trafficking - Minor");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+            case R.id.mSexTraffickingAdult:
+//                currentContent = "SexTraffickingAdult";
+                con.setcurrentContent(SEX_TRAFFICKING_ADULT);
+                this.setTitle("Sex Trafficking - Adult");
+                tvSectionName.setText("Sex Trafficking - Adult");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+            case R.id.mLaborTrafficking:
+//                currentContent = "LaborTrafficking";
+                con.setcurrentContent(LABOR_TRAFFICKING);
+                this.setTitle("Labor Trafficking");
+                tvSectionName.setText("Labor Trafficking");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+            case R.id.mSextortion:
+//                currentContent = "Sextortion";
+                con.setcurrentContent(SEXTORTION);
+                this.setTitle("Sextortion");
+                tvSectionName.setText("Sextortion");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+            case R.id.mChildPornography:
+//                currentContent = "ChildPornography";
+                con.setcurrentContent(CHILD_PORNOGRAPHY);
+                this.setTitle("Child Pornography");
+                tvSectionName.setText("Child Pornography");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+            case R.id.mSocialMediaExploitation:
+//                currentContent = "SocialMediaExploitation";
+                con.setcurrentContent(SOCIAL_MEDIA_EXPLOITATION);
+                this.setTitle("Social Media Exploitation");
+                tvSectionName.setText("Social Media Exploitation");
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                return true;
+        }
+        return false;
+    }
+
+    //button listeners
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.bSummary:
+                tvCurrentContent.setText(con.summaryContent());
+                changeButtonColors(bSummary, prevButton);
+                break;
+            case R.id.bGroomingProcess:
+                tvCurrentContent.setText(con.groomingProcessContent());
+                changeButtonColors(bGroomingProcess, prevButton);
+                break;
+            case R.id.bWarrningSigns:
+                tvCurrentContent.setText(con.warningSignsContent());
+                changeButtonColors(bWarrningSigns, prevButton);
+                break;
+            case R.id.bPreventMeasure:
+                tvCurrentContent.setText(con.preventativeMeasureContent());
+                changeButtonColors(bPreventMeasure, prevButton);
+                break;
+        }
+    }
+
+    //change the buttons color
+    private void changeButtonColors(Button blue, Button gray)
+    {
+        int lighGray = Color.parseColor("#a7afbc");
+        gray.setBackgroundColor(lighGray);
+        blue.setBackgroundColor(Color.BLUE);
+        prevButton = blue;
+    }
 }
