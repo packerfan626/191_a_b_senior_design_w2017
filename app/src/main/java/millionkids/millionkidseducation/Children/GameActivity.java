d0e0d536ad41422f8db469dfa41f780e3970cd5d
packerfan626 <dori_mouawad@hotmail.com>
2017-04-20 18:48:22 -0700

    Creating Game, SQLite Files

--- app/src/main/java/millionkids/millionkidseducation/Children/GameActivity.java
+++ app/src/main/java/millionkids/millionkidseducation/Children/GameActivity.java
@@ -3,13 +3,18 @@ package millionkids.millionkidseducation.Children;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import millionkids.millionkidseducation.R;
 
 public class GameActivity extends AppCompatActivity {
     //Used to set the background image from the SQLite Database
     ImageView background = (ImageView)findViewById(R.id.bgImage);
+
+    //Game Text that stores background information and questions
+    TextView gameText = (TextView)findViewById(R.id.gameText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,8 @@ public class GameActivity extends AppCompatActivity {
 
         //Set screen orientation to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //Setting scrollable for GameText TextView
+        gameText.setMovementMethod(new ScrollingMovementMethod());
     }
 }
---
+++ app/src/main/java/millionkids/millionkidseducation/SQLite/GameData.java
@@ -0,0 +1,9 @@
+package millionkids.millionkidseducation.SQLite;
+
+/**
+ * Created by dorimouawad on 4/20/17.
+ */
+
+//Stores GameData from SQLite
+public class GameData {
+}
