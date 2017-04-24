dae98046acc18004483d23dfd12bb7895f40957a
packerfan626 <dori_mouawad@hotmail.com>
2017-04-23 22:10:42 -0700

    Setting radio buttons and prepping for displaying data

--- app/src/main/java/millionkids/millionkidseducation/Children/GameActivity.java
+++ app/src/main/java/millionkids/millionkidseducation/Children/GameActivity.java
@@ -5,9 +5,11 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,6 +29,8 @@ public class GameActivity extends AppCompatActivity {
     RadioButton option1 = (RadioButton)findViewById(R.id.option1);
     RadioButton option2 = (RadioButton)findViewById(R.id.option2);
     RadioButton option3 = (RadioButton)findViewById(R.id.option3);
+
+    RadioGroup radioGroup;
 
     //Submit Option
     ImageButton submit = (ImageButton)findViewById(R.id.submitButton);
@@ -55,5 +59,48 @@ public class GameActivity extends AppCompatActivity {
 
         //Get GameData depending on ScenarioID
         games = gameData.getGameData(scenarioId);
+
+
+        //Add Listener for radio buttons
+        addListenerButton();
+    }
+
+    //Might not use
+    public void onRadioButtonClicked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()){
+            case R.id.option1:
+                if(checked){
+
+                }
+                break;
+            case R.id.option2:
+                if(checked){
+
+                }
+                break;
+            case R.id.option3:
+                if(checked){
+
+                }
+                break;
+        }
+    }
+
+    public void addListenerButton(){
+        radioGroup = (RadioGroup)findViewById(R.id.radio);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                selectedId = selectedId + 1;
+            }
+        });
+    }
+
+    public void displayData(){
+
     }
 }
